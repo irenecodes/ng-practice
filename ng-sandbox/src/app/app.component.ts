0f8b72facb15a98df291793237f2092044177930
Irene <irenesis123@gmail.com>
2021-02-01 12:03:09 -0500

    adding ngmodel 2way data binding from docs

--- ng-sandbox/src/app/app.component.ts
+++ ng-sandbox/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Item } from './item';
 
 
 @Component({
@@ -86,8 +86,55 @@ export class AppComponent {
       name: 'Yukon',
       taxPercentage: 5
     }
+
+
     
   ]
+  canSave =  true;
+  isSpecial = true;
+  isUnchanged = true;
+
+  currentClasses: {};
+
+  item: Item; // defined to demonstrate template context precedence
+  items: Item[];
+
+  currentItem: Item;
+
+  
+
+
+  ngOnInit() {
+    this.resetItems();
+    this.setCurrentClasses();
+    // this.setCurrentStyles();
+    // this.itemsNoTrackByCount = 0;
+  }
+
+  setCurrentClasses() {
+    // CSS classes: added/removed per current state of component properties
+    this.currentClasses =  {
+      saveable: this.canSave,
+      modified: !this.isUnchanged,
+      special:  this.isSpecial
+    };
+  }
+
+  resetItems() {
+    this.items = Item.items.map(item => item.clone());
+    this.currentItem = this.items[0];
+    this.item = this.currentItem;
+  }
+
+  
+  // setCurrentStyles() {
+  //   // CSS styles: set per current state of component properties
+  //   this.currentStyles = {
+  //     'font-style':  this.canSave      ? 'italic' : 'normal',
+  //     'font-weight': !this.isUnchanged ? 'bold'   : 'normal',
+  //     'font-size':   this.isSpecial    ? '24px'   : '12px'
+  //   };
+  // }
 
   // for template driven form 
   onSubmitTemplateBased(user) {
